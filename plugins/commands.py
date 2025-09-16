4aaf2bb21721d7707c0810a7569321fa89d71740
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2025-09-16 23:26:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,11 +100,14 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
+                    InlineKeyboardButton('ꜱᴇᴀʀᴄʜ ᴛɪᴘꜱ 🔍', url=f'https://telegra.ph/MOVIE-REQUEST-TIPS-08-21'),
+                    InlineKeyboardButton('ᴀʙᴏᴜᴛ ℹ️', callback_data='about')
                 ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                    InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    InlineKeyboardButton('ᴏᴡɴᴇʀ 👑', url=f'http://t.me/FilmyflixHD_Supportbot'),
+                    InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ 🌿', url=f'http://t.me/FilmyflixHD')
+                ],[
+                    InlineKeyboardButton('ᴛʀᴇɴᴅɪɴɢ 🔥', callback_data="topsearch"),
+                    InlineKeyboardButton('ᴘʀᴇᴍɪᴜᴍ 💎', callback_data="premium_info")
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
@@ -132,11 +135,11 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
+                    InlineKeyboardButton(' ꜱᴇᴀʀᴄʜ ᴛɪᴘꜱ', url=f'https://telegra.ph/MOVIE-REQUEST-TIPS-08-21'),
                     InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
                 ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                    InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    InlineKeyboardButton('ᴛʀᴇɴᴅɪɴɢ', callback_data="topsearch"),
+                    InlineKeyboardButton('ᴘʀᴇᴍɪᴜᴍ', callback_data="premium_info"),
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
