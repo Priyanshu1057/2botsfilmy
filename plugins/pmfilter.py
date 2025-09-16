bd175f73fed6d9ff2076d2a5cf4c7e47199415e8
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2025-09-16 23:31:15 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1407,11 +1407,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
                     InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
+                    InlineKeyboardButton('ꜱᴇᴀʀᴄʜ ᴛɪᴘꜱ 🔍', url=f'https://telegra.ph/MOVIE-REQUEST-TIPS-08-21'),
+                    InlineKeyboardButton('ᴀʙᴏᴜᴛ ℹ️', callback_data='about')
                 ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                     InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    InlineKeyboardButton('ᴏᴡɴᴇʀ 👑', url=f'http://t.me/FilmyflixHD_Supportbot'),
+                    InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ 🌿', url=f'http://t.me/FilmyflixHD')
+                ],[
+                    InlineKeyboardButton('ᴛʀᴇɴᴅɪɴɢ 🔥', callback_data="topsearch"),
+                    InlineKeyboardButton('ᴘʀᴇᴍɪᴜᴍ 💎', callback_data="premium_info")
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
@@ -1474,8 +1477,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('‼️ ᴅɪꜱᴄʟᴀɪᴍᴇʀ ‼️', callback_data='disclaimer'),
-            InlineKeyboardButton ('🪔 sᴏᴜʀᴄᴇ', callback_data='source'),
+            InlineKeyboardButton('‼️ ᴅɪꜱᴄʟᴀɪᴍᴇʀ ‼️', url=f'https://telegra.ph/Disclaimer-08-03-19'),
+            InlineKeyboardButton ('🪔 sᴏᴜʀᴄᴇ', url=f'https://t.me/+sE5BYFSoe9YxNDJl'),
         ],[
             InlineKeyboardButton('ᴅᴏɴᴀᴛɪᴏɴ 💰', callback_data='donation'), 
         ],[
