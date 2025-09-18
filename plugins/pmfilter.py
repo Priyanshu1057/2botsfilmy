4b6c8af1d22078f86111ebc10d5e97eb5e45d6a6
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2025-09-18 14:33:22 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1577,11 +1577,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
                     InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
+                    InlineKeyboardButton('ꜱᴇᴀʀᴄʜ ᴛɪᴘꜱ 🔍', url=f'https://telegra.ph/MOVIE-REQUEST-TIPS-08-21'),
+                    InlineKeyboardButton('ᴀʙᴏᴜᴛ ℹ️', callback_data='about')
                 ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                     InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    InlineKeyboardButton('ᴏᴡɴᴇʀ 👑', url=f'http://t.me/FilmyflixHD_Supportbot'),
+                    InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ 🌿', url=f'http://t.me/FilmyflixHD')
+                ],[
+                    InlineKeyboardButton('ᴛʀᴇɴᴅɪɴɢ 🔥', callback_data="topsearch"),
+                    InlineKeyboardButton('ᴘʀᴇᴍɪᴜᴍ 💎', callback_data="premium_info")
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
@@ -2237,3 +2240,4 @@ async def advantage_spell_chok(client, message):
     await d.delete()
     try: await message.delete()
     except: pass
+
