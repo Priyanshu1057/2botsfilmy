699a164c30d35cc96800df489fbd132a66659923
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2025-12-23 17:02:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/FilmyflixHDsupportgroup
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1001772561772")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1001647411072 -1001666034664")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS", "-1001647411072 -1001666034664 -1001732234578")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
