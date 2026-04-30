9183b6c254d44c99fd4bf26ed893363b1e2bc2da
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2026-04-30 17:09:18 +0530

    Update caption format in Script.py

--- Script.py
+++ Script.py
@@ -225,7 +225,10 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/FilmyflixHD">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/FilmyflixHD">[ FilmyflixHD ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/FilmyflixHD">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/FilmyflixHD">[ FilmyflixHD ]</a></b>
+🔞🔞 18+ bot-
+@xContentForYoubot
+@xContentForYoubot"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
